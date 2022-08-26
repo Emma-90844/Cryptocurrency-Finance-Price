a651bba7b51a4cdc61a3af6ef846db83b4d13389
Emma-90844 <enabojohn@gmail.com>
2022-08-26 16:32:33 +0300

    Prep to deploy on netlify

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Coin from "./Coin";
+import './App.css'
+
+
+function App() {
+
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    // function to fetch coin detail
+    const fetchCoins = () => {
+      axios
+        .get(
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        )
+        .then((res) => {
+          setCoins(res.data);
+        })
+        .catch((error) => console.log(error));
+    };
+    fetchCoins();
+  }, []);
+
+  // Handle change
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  // Filter function
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+  return (
+    <div className="coin-app">
+      <div className="coin-search">
+        <h className="coin-text">Cryptocurrency Finance Price Tracker App</h>
+        <form>
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="Search a Currency"
+            className="coin-input"
+          />
+        </form>
+      </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            Symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/login/login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function login() {
+  return (
+    <div className='login-container'>
+        <div className='login-box'>
+            <img alt='' src='https://www.google.com/url?sa=i&url=https%3A%2F%2Ftime.com%2F6115300%2Fcryptocurrency-value-3-trillion%2F&psig=AOvVaw3OhQS4cJh46PzRH5n8Bl-P&ust=1652810172675000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJj0xfTL5PcCFQAAAAAdAAAAABAF'/>
+        </div>
+    </div>
+  )
+}
+
+export default login
